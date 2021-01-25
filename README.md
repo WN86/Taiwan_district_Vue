3029d32d085e3a20707ec7ed9039d65eb23a0db0
WN86 <cantbackdown86@gmail.com>
2021-01-25 11:45:37 +0800

    init, add TW districts drop-down lists ,add README

--- README.md
+++ README.md
@@ -1,24 +1,17 @@
-# test
+## About Taiwan_district_Vue
 
-## Project setup
-```
-npm install
-```
+主要使用 Vue.js 建立一個可查詢台灣行政區域的下拉選單網頁。
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+### 功能
 
-### Compiles and minifies for production
-```
-npm run build
-```
+共有 2 個下拉選單：
+- 第 1 個選單：選取台灣的某縣市。
+- 第 2 個選單：依據選取的台灣縣市，只顯示該縣市管轄的區域供選擇。
 
-### Lints and fixes files
-```
-npm run lint
-```
+### 未來功能規劃
+- 可使用 Google Map Api 呈現所查詢區域在地圖上的位置。
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### 開發環境
+
+- 框架：Vue.js
+- 開發工具：Vue CLI
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="row no-gutters">
+      <!-- 選擇所在區域 -->
+      <div class="toolbox col-sm-3 p-2 bg-white">
+        <div class="form-group d-flex">
+          <label for="cityName" class="col-form-label mr-2 text-right">縣市</label>
+          <div class="flex-fill">
+            <select id="cityName" class="form-control" v-model="select.city">
+    <option value="">請選擇縣市</option>
+    <option :value="c.CityName" v-for="c in cityName" :key="c.CityName">
+      {{ c.CityName }}
+    </option>
+  </select>
+          </div>
+        </div>
+        <div class="form-group d-flex">
+          <label for="area" class="col-form-label mr-2 text-right">地區</label>
+          <div class="flex-fill">
+            <select id="area" class="form-control" v-model="select.area">
+    <option value="">請選擇地區</option>
+    // 建立專案時若有選擇 Airbnb 相關規範設定的話
+    // 需留意過長的程式碼會造成 line length error，換行即可解決此問題
+    <option :value="a.AreaName" v-for="a in cityName.find((city) =>
+    city.CityName === select.city).AreaList" :key="a.AreaName">
+      {{ a.AreaName }}
+    </option>
+  </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- 顯示藥局位置 -->
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// 導入內部檔案
+import cityName from './assets/CityCountyData.json';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  // 製作元件
+  data: () => ({
+    cityName,
+    // select 先暫時設定為空物件
+    select: {},
+  }),
+  // ...
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+  #map {
+    position: relative;
+    height: 100vh;
+  }
 </style>
